42027e8e01de941b46d4327b893a6ca3a4d3252f
Chihoon <achoon91@gmail.com>
2021-02-16 22:18:15 +0900

    Q4

---
+++ CodePractice1/src/chap2/Q4.java
@@ -0,0 +1,43 @@
+package chap2;
+import java.util.Scanner;
+// 배열 b의 모든 요소를 배열 a에 복사함
+
+public class Q4 {
+	static void copy(int[] a, int[] b)
+	{
+		int num=a.length<=b.length?a.length:b.length;
+		for(int i=0;i<num;i++)
+		{
+			a[i]=b[i];
+		}
+	}
+	public static void main(String[] args) {
+		Scanner stdIn=new Scanner(System.in);
+		
+		System.out.println("a의 요솟수는:");
+		int numa =stdIn.nextInt();
+		int[] a=new int[numa];
+		for(int i=0;i<numa;i++)
+		{
+			System.out.println("a["+i+"]:");
+			a[i] = stdIn.nextInt();
+		}
+		
+		System.out.println("b의 요솟수는:");
+		int numb=stdIn.nextInt();
+		int[] b=new int[numb];
+		for(int i=0;i<numb;i++)
+		{
+			System.out.println("b["+i+"]:");
+			b[i]=stdIn.nextInt();
+		}
+		
+		copy(a,b); //배열 b의 모든 요소를 배열 a에 역순으로 복사
+		
+		System.out.println("배열 b의 모든 요소를 배열 a에 복사했습니다");
+		for(int i=0;i<numa;i++)
+		{
+			System.out.println("a["+i+"]="+a[i]);
+		}
+	}
+}
